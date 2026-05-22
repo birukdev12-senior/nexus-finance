cc2a65e0f4d9b3100dc8f66b683af51eef8002bc
Biruk <btsegaye012@gmail.com>
2026-05-22 12:21:32 +0000

    fix authOptions import path

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/authOptions";
+import { authOptions } from "../../lib/authOptions";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
